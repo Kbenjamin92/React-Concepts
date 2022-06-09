bfb79f085710846e9fc19ac34a54f110ce3ffa71
Kbenjamin92 <kipplbenjamin@gmail.com>
2022-06-09 11:47:57 -0400

    play code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,77 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import axios from "axios";
 
-const App = () => {
-  const [display, setDisplay] = useState('');
+export default function App() {
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState([]);
+  const [apiData, setApiData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const request = axios.get("https://jsonplaceholder.typicode.com/posts/");
+      const response = await request;
+      setApiData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  let shortenedApiList = apiData.slice(0, 10);
+
+  const apiList = shortenedApiList.map((item) => {
+    return <div key={item.id}>{item.title}</div>;
+  });
+
+  useEffect(() => {
+    getData();
+  });
+
+  const list = items.map((items, keys) => {
+    return (
+      <div key={keys}>
+        <p>{items}</p>
+      </div>
+    );
+  });
+
+  const addItem = () => {
+    setItems([value, ...items]);
+    setValue("");
+  };
+  const removeAllItems = () => {
+    setItems([]);
+  };
 
   return (
     <div className="App">
-    
-      <MyButton onClick={() => setDisplay('Hello React!')}>
-        <p>Display Text</p>
-      </MyButton>
-      <br/>      
-      <MyButton onClick={() => setDisplay('')}>
-        <p>Remove Text</p>
-      </MyButton>
-
-      <p>{display}</p>
+      <h1>Grocery List</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <MyButton onClick={addItem}>Add Item</MyButton>
+      <MyButton onClick={removeAllItems}>Remove Items</MyButton>
+      {list}
+      {apiList}
     </div>
   );
 }
 
-export default App;
-
-// reusable custom button component
 const MyButton = (props) => {
-  const useCustomButtonStyles = {
-    backgroundColor: 'gold',
-    color: 'black',
-    border: '1px solid transparent',
-    borderRadius: '5px',
-    margin: '5px',
-  }
+  const useStyles = {
+    backgroundColor: "gold",
+    color: "black",
+    margin: "2px",
+    border: "1px solid transparent",
+    borderRadius: "5px"
+  };
+
   return (
     <>
-      <button 
-      style={useCustomButtonStyles}
-      onClick={props.onClick}
-      >
-      {props.children}
+      <button onClick={props.onClick} style={useStyles}>
+        {props.children}
       </button>
     </>
-  )};
-
-  
+  );
+};
